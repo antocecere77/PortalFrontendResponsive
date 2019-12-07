d82a962a4bbba1b32c4e7253af78f0c30bde94b4
antocecere <antocecere77@gmail.com>
2019-12-07 16:40:59 +0100

    Added download functionality

--- src/app/pages/components/form-stuff/form-validation/form-validation.component.ts
+++ src/app/pages/components/form-stuff/form-validation/form-validation.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as fileSaver from 'file-saver'; // npm i --save file-saver
+import { DownloadService } from 'src/app/shared/services/download.service';
+
 @Component({
   selector: 'app-form-validation',
   templateUrl: './form-validation.component.html',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormValidationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private downloadService: DownloadService) {
+  }
 
   ngOnInit() {
+  }
+
+  fileSystemName: string;
+  classpathFileName: string;
+
+  downloadFileSystem() {
+    this.downloadService.downloadFileSystem(this.fileSystemName)
+      .subscribe(response => {
+        const filename = response.headers.get('filename');
+
+        this.saveFile(response.body, filename);
+      });
+  }
+
+  downloadClasspathFile() {
+    this.downloadService.downloadClasspathFile(this.classpathFileName)
+      .subscribe(response => {
+        const filename = response.headers.get('filename');
+
+        this.saveFile(response.body, filename);
+      });
+  }
+
+  saveFile(data: any, filename?: string) {
+    const blob = new Blob([data], {type: 'text/csv; charset=utf-8'});
+    fileSaver.saveAs(blob, filename);
   }
 
 }
